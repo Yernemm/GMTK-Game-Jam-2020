0096e2aa4257df87aa9d3019f6629dae6d875b36
Yernemm <yrnemmizer@gmail.com>
2020-07-10 22:23:26 +0100

    some stuff
    has been worked on

--- Assets/PlayerInteracting.cs
+++ Assets/PlayerInteracting.cs
@@ -6,6 +6,11 @@ public class PlayerInteracting : MonoBehaviour
 {
 
     public Camera mainCam;
+    public LayerMask interactableMask;
+
+    public float interactDistance = 4f;
+
+    private RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,14 @@ public class PlayerInteracting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(checkInteraction()){
+            Debug.Log(hit.collider.gameObject);
+        }
+    }
+
+    bool checkInteraction() {
+        var ray =  Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        //canHookBool = Physics.Raycast(ray, out hit, 250f, ~playerMask);
+        return Physics.Raycast(ray, out hit, interactDistance, interactableMask);
     }
 }
