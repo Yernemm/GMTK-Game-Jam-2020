6a0ffafa9b56997eefd334faea3b49f16b8c3c27
Yernemm <yrnemmizer@gmail.com>
2020-07-11 20:09:08 +0100

    everybody have a wang chung tonight

---
+++ Assets/ExitTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTrigger : MonoBehaviour
+{
+
+    private LevelLoader levelLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.name == "Player"){
+            levelLoader.LoadNextLevel();
+        }
+    }
+}
