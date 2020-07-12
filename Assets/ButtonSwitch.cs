bbadf26ce738944769e4555cd5a356b50b27ade8
Yernemm <yrnemmizer@gmail.com>
2020-07-12 01:03:38 +0100

    dun dun DUN DUN

--- Assets/ButtonSwitch.cs
+++ Assets/ButtonSwitch.cs
@@ -11,6 +11,8 @@ public class ButtonSwitch : MonoBehaviour, IInteractable
 
     private Iswitchable sw;
 
+    public AudioSource soundEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,7 @@ public class ButtonSwitch : MonoBehaviour, IInteractable
 
     public void interact(GameObject obj){
         sw.switchTrigger(switchValue);
+        soundEffect.Play();
     }
 }
 
